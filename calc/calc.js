822d28fbda31384cb6ab1e7f709ce841d23dae7a
mexroj2603 <sayfullayevmexroj27@gmail.com>
2024-03-04 07:41:58 +0500

    04.03

--- calc/calc.js
+++ calc/calc.js
@@ -1,1 +1,24 @@
 select = "x";
+type = "+";
+x = document.querySelector(".x");
+y = document.querySelector(".y");
+z = document.querySelector(".z");
+ans = ducument.querySelector(".ans");
+
+btn_0 = document.querySelector(".btn_zero");
+btn_1 = document.querySelector(".btn_1");
+btn_2 = document.querySelector(".btn_2");
+btn_3 = document.querySelector(".btn_3");
+btn_4 = document.querySelector(".btn_4");
+btn_5 = document.querySelector(".btn_5");
+btn_6 = document.querySelector(".btn_6");
+btn_7 = document.querySelector(".btn_7");
+btn_8 = document.querySelector(".btn_8");
+btn_9 = document.querySelector(".btn_9");
+btn_plus = document.querySelector(".btn_plus");
+btn_minus = document.querySelector(".btn_minus");
+btn_plus_minus = document.querySelector(".btn_plus_minus");
+btn_multi = document.querySelector(".btn_multi");
+btn_div = document.querySelector(".btn_div");
+btn_eq = document.querySelector(".btn_eq");
+btn_percent = document.querySelector(".btn_percent");
